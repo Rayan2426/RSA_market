ecb79449801b2f817c63bef812902b811f9c1ddd
DevMushy <ragusamirko@gmail.com>
2024-05-27 12:29:55 +0200

    try commit

--- index.php
+++ index.php
@@ -117,7 +117,7 @@
                 $imageann = $conn->query($sql);
                 $imageann = $imageann->fetch_assoc()["urlImg"];
                 
-                echo "<div class='sale-box' onclick='showSale()'>
+                echo "<div class='sale-box' href='showsale.php?id=$idann'>
                         <img src='$imageann' width=300px>
                         <p> Nome: $title </p>
                         <p id='descrizione'> Descrizione: $desc </p>
---
+++ showsale.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>RSA Market | annuncio </title>
+</head>
+<body>
+    <div class="sale-container">
+        <div class="images-container">
+            <img src="#">
+            <img src="#">
+            <img src="#">
+        </div>
+        
+        <div class="selected-images">
+            
+        </div>
+
+
+    </div>
+</body>
+</html>
