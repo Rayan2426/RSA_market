b6058bed0d368f74f71b75f34d0d1fd79c283455
Daxe9 <davidexie694@gmail.com>
2024-05-27 12:57:09 +0200

    added an offer manager to manage new offers to sales

--- offermanager.php
+++ offermanager.php
@@ -7,11 +7,29 @@
 
     $method = $_POST["method"];
 
-    switch ($variable) {
+    switch ($method) {
         case 'create':
             $sum = $_POST["sum"];
             $saleid = $_POST["saleid"];
-            
+            $email = $_SESSION["email"];
+
+            if(!is_numeric($sum)){
+                redirect("index.php");
+            } else if( intval($sum) <= 0){
+                redirect("index.php");
+            }
+
+
+            $sql = "insert into Proposte(valore,Annuncio_ID,Stato,User_email)
+                    value($sum,$saleid,'available','$email')";
+            $conn->query($sql);
+
+            if($conn->affected_rows > 0){
+                echo "success";
+            } else{
+                echo "failure";
+            }
+
             break;
         
         default:
